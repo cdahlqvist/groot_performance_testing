a682a35b024d00431fd8b2dbcec25b0c336b10a5
Christian Dahlqvist <christian@elastic.com>
2016-01-21 17:24:43 +0000

    Added scripts for automate running of tests.

---
+++ run_all_tests.sh
@@ -0,0 +1,8 @@
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_20.conf -r all_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_40.conf -r all_40 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_elk2_20.conf -r elk2_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_lapd_20.conf -r lapd_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_acc_20.conf -r acc_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singadv_20.conf -r singadv_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singcar_20.conf -r singcar_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singinc_20.conf -r singinc_20 -D ./groot_data -i 0 -d 10
---
+++ run_background_test.sh
@@ -0,0 +1,1 @@
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_20.conf -D ./groot_data -i 0 -d 20
