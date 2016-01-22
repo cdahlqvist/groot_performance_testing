2ab7d7aa3efff29b31980b8956aa965adf654c72
Christian Dahlqvist <christian@elastic.com>
2016-01-22 07:18:20 +0000

    Updated and extended test suite.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,8 +1,11 @@
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_elk_20.json -r elk2_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_elk2_20.json -r elk2_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_lapd_20.json -r lapd_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_acc_20.json -r acc_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singadv_20.json -r singadv_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singcar_20.json -r singcar_20 -D ./groot_data -i 0 -d 5
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singinc_20.json -r singinc_20 -D ./groot_data -i 0 -d 5
 ./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_20.json -r all_20 -D ./groot_data -i 0 -d 10
 ./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_40.json -r all_40 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_elk2_20.json -r elk2_20 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_lapd_20.json -r lapd_20 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_acc_20.json -r acc_20 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singadv_20.json -r singadv_20 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singcar_20.json -r singcar_20 -D ./groot_data -i 0 -d 10
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_singinc_20.json -r singinc_20 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_60.json -r all_60 -D ./groot_data -i 0 -d 10
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_80.json -r all_80 -D ./groot_data -i 0 -d 10
--- run_background_test.sh
+++ run_background_test.sh
@@ -1,1 +1,1 @@
-./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_20.json -D ./groot_data -i 0 -d 20
+./bin/rankin -h $GROOT_HOST -p https -c $GROOT_USER:$GROOT_PWD -f./drivers/groot/config/groot_all_40.json -D ./groot_data -i 0 -d 20
