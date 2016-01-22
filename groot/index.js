3fe98f92e48e12fa57716b63d11f6b929a1ea143
Christian Dahlqvist <christian@elastic.com>
2016-01-22 10:54:17 +0000

    Updates to enable testing of additional elk dashboard variations.

--- groot/index.js
+++ groot/index.js
@@ -184,7 +184,7 @@ module.exports.elk2 = function(esClient, state, driver_data, operation_parameter
 }
 
 // This is a artificial dashboard where the highlight and sort clauses have been removed from the search panel query.
-module.exports.elk_revised = function(esClient, state, driver_data, operation_parameters, result_callback) {
+module.exports.elk_rev1 = function(esClient, state, driver_data, operation_parameters, result_callback) {
   var timeout = state.timeout;
   if(operation_parameters.timeout && util.is_integer(operation_parameters.timeout) && operation_parameters.timeout > 0) {
     timeout = operation_parameters.timeout;
@@ -224,6 +224,64 @@ module.exports.elk_revised = function(esClient, state, driver_data, operation_pa
     {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"terms":{"field":"response","size":5,"order":{"_count":"desc"}},"aggs":{"3":{"significant_terms":{"field":"geoip.country_name.raw","size":3}}}}}},
     {"index":index,"ignore_unavailable":true},
     {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":500,"fields":["*","_source"],"script_fields":{},"fielddata_fields":["@timestamp"]},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"terms":{"field":"request.raw","size":10,"order":{"_count":"desc"}}}}}
+  ];
+
+  esClient.msearch({
+    preference: end_ts.toString(),
+    body: bulk_body,
+    requestTimeout: timeout
+  }, function (err, resp) {
+    if (err) {
+      result_callback( { result_code: 'ERROR', visualizations: 8 } );
+    }
+
+    result_callback( { result_code: 'OK', visualizations: 8 } );
+  });
+}
+
+// This is a artificial dashboard where the highlight clause have been removed from the search panel query.
+module.exports.elk_rev2 = function(esClient, state, driver_data, operation_parameters, result_callback) {
+  var timeout = state.timeout;
+  if(operation_parameters.timeout && util.is_integer(operation_parameters.timeout) && operation_parameters.timeout > 0) {
+    timeout = operation_parameters.timeout;
+  }
+
+  var period = state.period;
+  if(operation_parameters.period && util.is_integer(operation_parameters.period) && operation_parameters.period > 0) {
+    period = operation_parameters.period;
+  }
+
+  var index = state.index;
+  if(operation_parameters.index && (util.is_string(operation_parameters.index) || util.is_string_array(operation_parameters.index))) {
+    index = operation_parameters.index;
+  }
+
+  var days = state.days;
+
+  if(operation_parameters.days && util.is_string(operation_parameters.days)) {
+    days = parse_days(operation_parameters.days);
+  }
+
+  var end_ts = _.random(days.start, days.end);
+  var start_ts = end_ts - (period * 24 * 3600 * 1000);
+
+  var bulk_body = [
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"12h","time_zone":"Europe/London","min_doc_count":1,"extended_bounds":{"min":start_ts,"max":end_ts}},"aggs":{"3":{"terms":{"field":"geoip.country_name.raw","size":5,"order":{"1":"desc"}},"aggs":{"1":{"sum":{"field":"bytes"}}}}}}}},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"geohash_grid":{"field":"geoip.location","precision":2}}}},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"cardinality":{"field":"clientip.raw"}}}},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"terms":{"field":"agent.raw","size":10,"order":{"_count":"desc"}}}}},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"12h","time_zone":"Europe/London","min_doc_count":1,"extended_bounds":{"min":start_ts,"max":end_ts}},"aggs":{"3":{"terms":{"field":"response","size":5,"order":{"_count":"desc"}}}}}}},
+    {"index":index,"search_type":"count","ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"terms":{"field":"response","size":5,"order":{"_count":"desc"}},"aggs":{"3":{"significant_terms":{"field":"geoip.country_name.raw","size":3}}}}}},
+    {"index":index,"ignore_unavailable":true},
+    {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":500,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"fields":["*","_source"],"script_fields":{},"fielddata_fields":["@timestamp"]},
     {"index":index,"search_type":"count","ignore_unavailable":true},
     {"query":{"filtered":{"query":{"query_string":{"query":"*","analyze_wildcard":true}},"filter":{"bool":{"must":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}},{"range":{"@timestamp":{"gte":start_ts,"lte":end_ts,"format":"epoch_millis"}}}],"must_not":[]}}}},"size":0,"aggs":{"2":{"terms":{"field":"request.raw","size":10,"order":{"_count":"desc"}}}}}
   ];
